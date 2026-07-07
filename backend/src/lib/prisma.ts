cb045595483bcab06befbb3b6376af44d3f3f217
Gabriel Brito <gabrielbrito.ufrrj@gmail.com>
2026-07-07 18:47:12 -0300

    fix(db): configurar adapter pg Prisma 7 e preparar schema para auth social

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,13 @@
+import { env } from '../env'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
+const connectionString = `${env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
 export const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'dev' ? ['query', 'error'] : ['error'],
+  adapter,
+  log: env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
 })
